5ba9de9783c3efb952f015c08effdd687c01bb4a
wjnunn12-source <wjnunn12@gmail.com>
2026-03-29 15:16:36 +1000

    will is a super cool guy

---
+++ gameScript.js
@@ -0,0 +1,54 @@
+
+
+
+const gameDirectories = ["games/geometry-dash", "games/minecraft"];
+const gameNames = ["Gemometry Dash", "Minecraft"];
+const  _gameTemplate = document.querySelector("#gameTemplate");
+
+let newGame
+for (let i of gameDirectories){
+    newGame = _gameTemplate.content.cloneNode(true);
+    newGame.querySelector(".gameFrame").setAttribute("data-src", i + "/index.html");
+    newGame.querySelector(".gameBg").setAttribute("style", `background-image: linear-gradient(to bottom, rgba(134, 42, 255, 0.5), rgba(0, 0, 0, 0.5)), url(${i + '/cover.png'})`);
+    newGame.querySelector(".gameTitle").textContent = gameNames[gameDirectories.indexOf(i)];
+    console.log(newGame.querySelector(".gameTitle"))
+    document.querySelector('#game-container').appendChild(newGame);
+    
+    
+}
+
+const startButtons = document.getElementsByClassName("startButton");
+const fullScreenButtons = document.getElementsByClassName("fullScreen");
+
+for (let i of startButtons){
+    i.addEventListener("click", (e) => startGame(e.target));
+}
+
+for (let i of fullScreenButtons){
+    i.addEventListener("click", (e) => fullScreen(e.target));
+}
+
+
+function startGame(element){
+    for ( let i of element.parentNode.children){
+        if (i.classList.contains("gameFrame")){
+            i.src = i.getAttribute("data-src");
+        }
+
+        if (i.classList.contains("gameBg")){
+            i.remove();
+        }
+        
+    }
+    element.remove();
+}
+
+
+function fullScreen(element){
+    console.log(element);
+    for ( let i of element.parentNode.children){
+        if (i.classList.contains("gameFrame")){
+            i.requestFullscreen();
+        }
+    }
+}
